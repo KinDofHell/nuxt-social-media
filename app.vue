321e32acd94b7adc168f992a750f4c8fe4a87f8e
kindofhell <ihordzhus24@gmail.com>
2024-07-23 12:51:00 +0300

    [FEATURE] Handle Refresh Token and Login Page
    - create login page
    - handle refresh token
    - refactor existed code

--- app.vue
+++ app.vue
@@ -7,21 +7,24 @@ const darkMode = ref(true);
 <template>
   <div :class="{'dark': darkMode}">
     <div class="bg-white dark:bg-dim-900">
-      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
-        <div class="hidden md:block xs-col-span-1 xl:col-span-2">
-          <div class="sticky top-0">
-            <LeftSidebar/>
+      <div v-if="false" class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <LeftSidebar/>
+            </div>
           </div>
-        </div>
-        <main class="col-span-12 md:col-span-8 xl:col-span-6">
-          <RouterView />
-        </main>
-        <div class="col-span-12 md:block xs-col-span-4 md:col-span-3">
-          <div class="sticky top-0">
-            <SidebarRight />
+          <main class="col-span-12 md:col-span-8 xl:col-span-6">
+            <RouterView />
+          </main>
+          <div class="col-span-12 md:block xs-col-span-4 md:col-span-3">
+            <div class="sticky top-0">
+              <SidebarRight />
+            </div>
           </div>
         </div>
       </div>
+      <AuthPage v-else />
     </div>
   </div>
 </template>
